6f2a0ca8389f16368c3085a4b36107ebf09a5c9b
ABoni14 <aboni150@gmail.com>
2021-12-13 12:26:11 +0100

    jumbotron

--- src/components/JumboTron.vue
+++ src/components/JumboTron.vue
@@ -1,6 +1,13 @@
 <template>
   <section class="container-1100">
-    <h2></h2>
+    <div class="left-section">
+      <h2>The biggest platform for your growth.</h2>
+      <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form</p>
+      <button class="button">Read More</button>
+    </div>
+    <div class="right-section">
+      <img src="../assets/img/27-1.png" alt="">
+    </div>
   </section>
 </template>
 
@@ -14,8 +21,34 @@ export default {
 @import "../assets/style/vars.scss";
 
 section{
-  height: 300px;
+  height: 380px;
   margin-top: 20px;
   background-color: $secondary-bg;
+  .left-section,
+  .right-section{
+    float: left;
+    width: 50%;
+  }
+  .left-section{
+    padding: 45px;
+    h2{
+      font-size: 50px;
+      font-weight: 400;
+      line-height: 70px;
+      padding-bottom: 20px;
+    }
+    p{
+      color: $secondary-text-color;
+      line-height: 25px;
+      padding-bottom: 20px;
+    }
+  }
+  .right-section{
+    padding: 20px;
+    img{
+      width: 100%;
+    }
+  }
+  
 }
 </style>
